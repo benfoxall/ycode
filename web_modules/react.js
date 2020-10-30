d3168538b54af94683be6c7f3ec9935317b581b0
benfoxall <benfoxall@gmail.com>
2020-10-30 17:57:04 +0000

    Deploying to gh-pages from  @ e14ae8c3aff2906bd343eca3b5b9d712c0b113c7 🚀

--- web_modules/react.js
+++ web_modules/react.js
@@ -3,7 +3,8 @@ export { r as default } from './common/index-6ed86a98.js';
 
 
 
+var createContext = react.createContext;
 var useEffect = react.useEffect;
 var useRef = react.useRef;
 var useState = react.useState;
-export { useEffect, useRef, useState };
+export { createContext, useEffect, useRef, useState };
