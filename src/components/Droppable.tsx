2a7f492939425c843fb63c47e8873fab71bce06d
Ben Foxall <benfoxall@gmail.com>
2020-10-31 10:59:23 +0000

    Prettier

--- src/components/Droppable.tsx
+++ src/components/Droppable.tsx
@@ -1,44 +1,45 @@
+import React, {
+  createContext,
+  DragEventHandler,
+  FunctionComponent,
+  useState,
+} from 'react';
 
-import React, {createContext, DragEventHandler, FunctionComponent, useState} from 'react'
-
-const DraggingCtx = createContext(false)
+const DraggingCtx = createContext(false);
 
 interface Props {
-    onFile: (handle: FileSystemFileHandle) => void;
+  onFile: (handle: FileSystemFileHandle) => void;
 }
 
-export const Droppable: FunctionComponent<Props> = ({children, onFile}) => {
+export const Droppable: FunctionComponent<Props> = ({ children, onFile }) => {
+  const [dragging, setDragging] = useState(false);
 
-    const [dragging, setDragging] = useState(false);
+  const drop: DragEventHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const file = e.dataTransfer.items[0];
 
-    const drop: DragEventHandler = async (e) => {
-      e.preventDefault();
-      try {
-        const file = e.dataTransfer.items[0];
-  
-        const handle = await file.getAsFileSystemHandle();
-  
-        onFile(handle as FileSystemFileHandle);
-      } catch (e) {
-        console.error('err', e);
-      }
-    };
-  
-    const drag: DragEventHandler = (e) => {
-      e.preventDefault();
-      setDragging(true);
-    };
-  
-    const dragEnd: DragEventHandler = (e) => {
-      e.preventDefault();
-      setDragging(false);
-    };
+      const handle = await file.getAsFileSystemHandle();
 
-    return (
+      onFile(handle as FileSystemFileHandle);
+    } catch (e) {
+      console.error('err', e);
+    }
+  };
+
+  const drag: DragEventHandler = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const dragEnd: DragEventHandler = (e) => {
+    e.preventDefault();
+    setDragging(false);
+  };
+
+  return (
     <div className="App" onDrop={drop} onDragOver={drag} onDragLeave={dragEnd}>
-        <DraggingCtx.Provider value={dragging}>
-          {children}
-        </DraggingCtx.Provider>
+      <DraggingCtx.Provider value={dragging}>{children}</DraggingCtx.Provider>
     </div>
-    )
-}
+  );
+};
