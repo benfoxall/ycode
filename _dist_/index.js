a4a01e4fb0a4a3dc53ab739783db8ce95b1b26d8
benfoxall <benfoxall@gmail.com>
2020-11-01 23:08:06 +0000

    Deploying to gh-pages from  @ 633c527a4e5000869af1ff643d2ca4fe4054b4ff 🚀

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,11 +1,1 @@
-import __SNOWPACK_ENV__ from '../__snowpack__/env.js';
-import.meta.env = __SNOWPACK_ENV__;
-
-import React from "../web_modules/react.js";
-import ReactDOM from "../web_modules/react-dom.js";
-import App2 from "./App.js";
-import "./index.css.proxy.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App2, null)), document.getElementById("root"));
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
+import t from"../__snowpack__/env.js";import.meta.env=t;import e from"../web_modules/react.js";import o from"../web_modules/react-dom.js";import m from"./App.js";import"./index.css.proxy.js";o.render(e.createElement(e.StrictMode,null,e.createElement(m,null)),document.getElementById("root")),import.meta.hot&&import.meta.hot.accept();
--- web_modules/y-protocols/awareness.js
+++ web_modules/y-protocols/awareness.js
@@ -1,2 +1,1 @@
-import '../common/function-d4886006.js';
-export { A as Awareness } from '../common/awareness-96ffb3f7.js';
+import"../common/function-d4886006.js";export{A as Awareness}from"../common/awareness-96ffb3f7.js";
