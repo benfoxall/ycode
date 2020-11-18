23bba15e25ba51a9ce522e813cba3662c8e6f62e
benfoxall <benfoxall@gmail.com>
2020-11-18 09:52:17 +0000

    Deploying to gh-pages from  @ 9af249851136aa39773afbc57ba106dbaf88df89 🚀

--- _dist_/components/monaco.js
+++ _dist_/components/monaco.js
@@ -3,8 +3,8 @@ import {useMediaQuery} from "./hooks.js";
 const baseUrl = "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.21.2/min";
 const integrity = "sha512-dx6A3eMO/vvLembE8xNGc3RKUytoTIX3rNO5uMEhzhqnXYx1X5XYmjfZP7vxYv7x3gBhdj7Pgys8DUjdbDaLAA==";
 const monacoPromised = (async () => {
-  const wat = window;
-  if (!wat.require) {
+  const win = window;
+  if (!win.require) {
     const scr = document.createElement("script");
     scr.setAttribute("src", `${baseUrl}/vs/loader.min.js`);
     scr.setAttribute("integrity", integrity);
@@ -12,23 +12,19 @@ const monacoPromised = (async () => {
     const load = new Promise((res) => scr.addEventListener("load", res));
     document.head.append(scr);
     await load;
-    console.log("loaded require");
   }
-  if (!wat.monaco) {
-    wat.require.config({paths: {vs: `${baseUrl}/vs`}});
-    wat.MonacoEnvironment = {getWorkerUrl: () => proxy};
+  if (!win.monaco) {
+    win.require.config({paths: {vs: `${baseUrl}/vs`}});
+    win.MonacoEnvironment = {getWorkerUrl: () => proxy};
     let proxy = URL.createObjectURL(new Blob([
       `
-        self.MonacoEnvironment = {
-            baseUrl: '${baseUrl}'
-        };
+        self.MonacoEnvironment = { baseUrl: '${baseUrl}' };
         importScripts('${baseUrl}/vs/base/worker/workerMain.min.js');
-    `
+        `
     ], {type: "text/javascript"}));
-    await new Promise((res) => wat.require(["vs/editor/editor.main"], res));
-    console.log("loaded monaco");
+    await new Promise((res) => win.require(["vs/editor/editor.main"], res));
   }
-  return wat.monaco;
+  return win.monaco;
 })();
 export const useMonaco = () => {
   const ref = useRef(null);
